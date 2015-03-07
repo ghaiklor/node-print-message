79b76973a673389cb8932593b2d218a0f93753ef
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-07 19:54:49 +0200

    Add tests for print function

--- test/print.js
+++ test/print.js
@@ -1,8 +1,22 @@
 var assert = require('assert'),
+    BaseMessage = require('../lib/messages/BaseMessage'),
+    BorderedMessage = require('../lib/messages/BorderedMessage'),
     print = require('../');
 
 describe('module:index', function () {
     it('Should be a function', function () {
         assert.equal(typeof print, 'function');
+    });
+
+    it('Should properly instantiate BaseMessage', function () {
+        assert(print(['test'], {
+            border: false
+        }) instanceof BaseMessage);
+    });
+
+    it('Should properly instantiate BorderedMessage', function () {
+        assert(print(['test'], {
+            border: true
+        }) instanceof BorderedMessage);
     });
 });

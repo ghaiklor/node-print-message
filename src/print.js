498342176d666fac77499ddc3eabb7674bdbfca3
ghaiklor <ghaiklor@gmail.com>
2015-11-27 17:13:32 +0200

    fix(print): Removes default value from lines param as it is required argument

--- src/print.js
+++ src/print.js
@@ -7,7 +7,7 @@ import { BorderedMessage } from './messages/BorderedMessage';
  * @param {Array} lines Array of lines
  * @param {Object} [config] Additional params for print
  */
-export default function (lines = [], config = {}) {
+export default function (lines, config = {}) {
   if (config.border === false) {
     return new BaseMessage(lines, config).print();
   } else {
